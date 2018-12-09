bd7641560d90d1a35b1454eb4c24fe52fa845a60
StevenHibble <stevenhibble@gmail.com>
2018-12-09 00:49:07 -0700

    Finished Part Two

--- Day 02/Box IDs.py
+++ Day 02/Box IDs.py
@@ -23,3 +23,19 @@ print(f'The answer to Part 1 is: {tally_counts.count(2) * tally_counts.count(3)}
 
 
 ### Part Two: Find two similar strings
+from itertools import combinations
+
+# Give the # of characters that are different at the same position
+def string_difference(string1, string2):
+    return len([s for i, s in enumerate(string1) if s != string2[i]])
+
+# Give the characters that are the same at the same position
+def string_similarity(string1, string2):
+    return ''.join([s for i, s in enumerate(string1) if s == string2[i]])
+
+# combinations compares every element to every other element only once
+for s1, s2 in combinations(data, 2):
+    if string_difference(s1, s2) == 1:
+        break # s1 and s2 retain their last values
+
+print(f'The answer to Part 2 is: {string_similarity(s1, s2)}')
