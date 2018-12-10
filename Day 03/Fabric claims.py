7ed22204aed455ddd3793fce5ccf20230fc2709d
StevenHibble <stevenhibble@gmail.com>
2018-12-10 11:19:08 -0700

    Finished Part 1. Used"and" instead of "&" when testing sets

--- Day 03/Fabric claims.py
+++ Day 03/Fabric claims.py
@@ -15,9 +15,7 @@ class elfClaim:
 
         self.instructions = instructions
         self.id = inst[0]
-        # self.corners = [{'x': inst[1],               'y': inst[2]}, 
-        #                 {'x': inst[1] + inst[3] - 1, 'y': inst[2] + inst[4] - 1}]
-        self.dims = {'x': range(inst[1], inst[1] + inst[3]),
+        self.dims = {'x': range(inst[1], inst[1] + inst[3]), # Range isn't inclusive for the end
                      'y': range(inst[2], inst[2] + inst[4])}
 
     def __repr__(self):
@@ -30,7 +28,7 @@ class elfClaim:
         x_overlap = set(self.dims['x']).intersection(other.dims['x'])
         y_overlap = set(self.dims['y']).intersection(other.dims['y'])
 
-        if x_overlap & y_overlap:
+        if x_overlap and y_overlap:
             return {'x': range(min(x_overlap), max(x_overlap)+1),
                     'y': range(min(y_overlap), max(y_overlap)+1)}
 
@@ -40,14 +38,13 @@ claims = list(map(elfClaim, data))
 ### Part One
 from itertools import combinations, product, chain
 
-# fabric = [[0] * 1000 for i in range(1000)]
-
-
 #tests = [elfClaim('#1 @ 1,3: 4x4'), elfClaim('#2 @ 3,1: 4x4'), elfClaim('#3 @ 5,5: 2x2')]
 overlaps = []
 for c1, c2 in combinations(claims, 2):
     o = c1.overlap(c2)
+
     if o is not None:
+        print(f'{c1.id} :: {c2.id}: {o}')
         overlaps.append(o)
 
 def expandOverlapToCoords(overlap):
@@ -55,4 +52,5 @@ def expandOverlapToCoords(overlap):
     y = list(overlap['y'])
     return list(product(x, y))
 
-print(set(chain.from_iterable(map(expandOverlapToCoords, overlaps))))
+print(len(set(chain.from_iterable(map(expandOverlapToCoords, overlaps)))))
+#print(set(chain.from_iterable(map(expandOverlapToCoords, overlaps))))
